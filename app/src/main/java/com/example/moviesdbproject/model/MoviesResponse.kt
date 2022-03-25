1999a43a00a786b7835025a3c31d9432cae114ec
paisadallas <44854381+paisadallas@users.noreply.github.com>
2022-03-25 13:58:23 -0400

    added Model and gson library

---
+++ app/src/main/java/com/example/moviesdbproject/model/MoviesResponse.kt
@@ -0,0 +1,17 @@
+package com.example.moviesdbproject.model
+
+
+import com.google.gson.annotations.SerializedName
+
+data class MoviesResponse(
+    @SerializedName("dates")
+    val dates: Dates,
+    @SerializedName("page")
+    val page: Int,
+    @SerializedName("results")
+    val results: List<Movie>,
+    @SerializedName("total_pages")
+    val totalPages: Int,
+    @SerializedName("total_results")
+    val totalResults: Int
+)
