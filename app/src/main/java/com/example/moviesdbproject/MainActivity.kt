d81bc3d89a2f3744ec775786d6f9dc00be70e7db
inaki92 <45763109+inaki92@users.noreply.github.com>
2022-03-28 16:41:09 -0400

    DI using hilt (#9)
    * DI using hilt

--- app/src/main/java/com/example/moviesdbproject/MainActivity.kt
+++ app/src/main/java/com/example/moviesdbproject/MainActivity.kt
@@ -2,21 +2,10 @@ package com.example.moviesdbproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.lifecycle.ViewModelProvider
-import com.example.moviesdbproject.rest.MovieRepository
-import com.example.moviesdbproject.viewmodel.MovieViewModel
-import com.example.moviesdbproject.viewmodel.MoviesViewModelFactory
-import javax.inject.Inject
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-
-    @Inject
-    lateinit var movieRepository: MovieRepository
-
-    private val viewmodel by lazy {
-        ViewModelProvider(this, MoviesViewModelFactory(movieRepository))[MovieViewModel::class.java]
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
--- app/src/main/java/com/example/moviesdbproject/di/NetworkModule.kt
+++ app/src/main/java/com/example/moviesdbproject/di/NetworkModule.kt
@@ -5,15 +5,23 @@ import com.example.moviesdbproject.rest.MovieRepositoryImpl
 import com.example.moviesdbproject.rest.Services
 import dagger.Module
 import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
 
 @Module
+@InstallIn(SingletonComponent::class)
 class NetworkModule {
+
     @Provides
+    @Singleton
     fun providesNetworkService(okHttpClient: OkHttpClient) =
         Retrofit.Builder()
             .baseUrl(Services.BASE_URL)
@@ -23,6 +31,7 @@ class NetworkModule {
             .create(Services::class.java)
 
     @Provides
+    @Singleton
     fun providesOkHttpClient() =
         OkHttpClient.Builder()
             .addInterceptor(HttpLoggingInterceptor().apply {
@@ -36,4 +45,7 @@ class NetworkModule {
     @Provides
     fun providesMovieRepository(movieServices: Services): MovieRepository =
         MovieRepositoryImpl(movieServices)
+
+    @Provides
+    fun providesDispatcher(): CoroutineDispatcher = Dispatchers.IO
 }
