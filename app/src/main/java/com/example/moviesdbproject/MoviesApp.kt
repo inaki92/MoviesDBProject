ffdf7382c79741c92ad055b00d260b1a84219ac9
inaki <ironhide0905@gmail.com>
2022-03-28 16:17:44 -0400

    DI using hilt

--- app/src/main/java/com/example/moviesdbproject/MoviesApp.kt
+++ app/src/main/java/com/example/moviesdbproject/MoviesApp.kt
@@ -1,21 +1,7 @@
 package com.example.moviesdbproject
 
 import android.app.Application
-import com.example.moviesdbproject.di.ApplicationModule
-import com.example.moviesdbproject.di.DaggerMoviesComponent
-import com.example.moviesdbproject.di.MoviesComponent
+import dagger.hilt.android.HiltAndroidApp
 
-class MoviesApp : Application() {
-    override fun onCreate() {
-        super.onCreate()
-
-        moviesComponent = DaggerMoviesComponent
-            .builder()
-            .applicationModule(ApplicationModule(this))
-            .build()
-    }
-
-    companion object{
-        lateinit var moviesComponent: MoviesComponent
-    }
-}
+@HiltAndroidApp
+class MoviesApp : Application()
