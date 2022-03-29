8fe26de5535ca5e28766b5ea81da7acf62c8959e
inaki92 <45763109+inaki92@users.noreply.github.com>
2022-03-29 15:01:46 -0400

    adding kotlin flows and details movie call (#10)
    Co-authored-by: inaki <ironhide0905@gmail.com>

--- app/src/main/java/com/example/moviesdbproject/rest/MovieRepository.kt
+++ app/src/main/java/com/example/moviesdbproject/rest/MovieRepository.kt
@@ -1,28 +1,60 @@
 package com.example.moviesdbproject.rest
 
 import com.example.moviesdbproject.model.MoviesResponse
-import com.example.moviesdbproject.model.details.MoviesDetails
+import com.example.moviesdbproject.viewmodel.MovieState
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
 import retrofit2.Response
 
 interface MovieRepository {
-   suspend fun getPlayNowMovies(): Response<MoviesResponse>
+   val movieDetails: StateFlow<MovieState>
+   fun getPlayNowMovies(): Flow<MovieState>
    suspend fun getAllPopularMovies(page: Int, language : String = Services.LANGUAGE): Response<MoviesResponse>
-   suspend fun getAllDetailsMovies(movieId : Int): Response<MoviesDetails>
+   fun getAllDetailsMovies(movieId : Int, coroutineScope: CoroutineScope)
 }
 
 class MovieRepositoryImpl(
    private val movieServices: Services
 ):MovieRepository{
 
-   override suspend fun getPlayNowMovies(): Response<MoviesResponse> {
-     return movieServices.getAllPlayNowMovies()
+   private val _movieDetails: MutableStateFlow<MovieState> = MutableStateFlow(MovieState.LOADING)
+   override val movieDetails: StateFlow<MovieState>
+      get() = _movieDetails
+
+   override fun getPlayNowMovies(): Flow<MovieState> = flow {
+      try {
+         val response = movieServices.getAllPlayNowMovies()
+         if (response.isSuccessful){
+            response.body() ?.let {
+               emit(MovieState.SUCCESS(it))
+            } ?: throw Exception("Playing now movie no response")
+         } else {
+            throw Exception("Playing now movie unsuccessful")
+         }
+      } catch (e : Exception) {
+         emit(MovieState.ERROR(e))
+      }
    }
 
    override suspend fun getAllPopularMovies(page: Int,language: String): Response<MoviesResponse> {
      return movieServices.getAllPopularMovies(page = page, language = language)
    }
 
-   override suspend fun getAllDetailsMovies(movieId: Int): Response<MoviesDetails> {
-      return movieServices.getAllDetailsMovies(movieID = movieId)
+   override fun getAllDetailsMovies(movieId: Int, coroutineScope: CoroutineScope) {
+      coroutineScope.launch {
+         try {
+            val response = movieServices.getAllDetailsMovies(movieId)
+            if (response.isSuccessful){
+               response.body() ?.let {
+                  _movieDetails.value = MovieState.SUCCESS(it)
+               } ?: throw Exception("Movie details no response null")
+            } else {
+               throw Exception("Movie details unsuccessful")
+            }
+         } catch (e : Exception) {
+            _movieDetails.value = MovieState.ERROR(e)
+         }
+      }
    }
 }
--- app/src/main/java/com/example/moviesdbproject/viewmodel/MovieViewModel.kt
+++ app/src/main/java/com/example/moviesdbproject/viewmodel/MovieViewModel.kt
@@ -1,14 +1,9 @@
 package com.example.moviesdbproject.viewmodel
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.*
 import com.example.moviesdbproject.rest.MovieRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 import javax.inject.Inject
 
 @HiltViewModel
@@ -21,23 +16,22 @@ class MovieViewModel @Inject constructor (
     val moviesLiveData : LiveData<MovieState> get() = _moviesLiveData
 
     fun getPlayNowMovies() {
-
         _moviesLiveData.postValue(MovieState.LOADING)
 
         viewModelScope.launch(coroutineDispatcher) {
-            try {
-                val response = movieRepository.getPlayNowMovies()
-                if (response.isSuccessful){
-                    response.body() ?.let {
-                        _moviesLiveData.postValue(MovieState.SUCCESS(it))
-                    }?: throw Exception("playing now movie no response")
+            movieRepository.getPlayNowMovies()
+                .collect { state ->
+                    _moviesLiveData.postValue(state)
                 }
-                else {
-                    throw Exception("playing now movie unsuccessful")
-                }
-            } catch (e : Exception){
-                _moviesLiveData.postValue(MovieState.ERROR(e))
+        }
+    }
+
+    fun getMovieDetails(movieId: Int) {
+        viewModelScope.launch(coroutineDispatcher) {
+            movieRepository.movieDetails.collect {
+                // todo posting to the livedata
             }
         }
+        movieRepository.getAllDetailsMovies(movieId, viewModelScope)
     }
 }
